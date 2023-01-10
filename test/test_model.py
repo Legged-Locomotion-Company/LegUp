896a05d3e4271ddd802682c347495adc5a97b7de
andrew mead <ammead@umass.edu>
2023-01-10 03:14:28 -0500

    Add tests for teacher model

---
+++ test/test_model.py
@@ -0,0 +1,29 @@
+import torch
+from rlloco.agents.models.anymal.teacher import Teacher
+
+
+def test_confirm_teacher_dimensions():
+    """
+    Confirm that the teacher dimensions are correct.
+    """
+    teacher = Teacher(None)
+    actor, critic = teacher(torch.rand([1, 391]))
+    assert actor.shape == torch.Size([1, 16]), "actor should be shape (1, 16), but is {actor_shape}".format(
+        actor_shape=actor.shape)
+
+    assert critic.shape == torch.Size([1, 1]), "critic should be shape (1, 1), but is {critic_shape}".format(
+        critic_shape=critic.shape)
+
+    actor, critic = teacher(torch.rand([2, 391]))
+    assert actor.shape == torch.Size([2, 16]), "actor should be shape (2, 16), but is {actor_shape}".format(
+        actor_shape=actor.shape)
+
+    assert critic.shape == torch.Size([2, 1]), "critic should be shape (2, 1), but is {critic_shape}".format(
+        critic_shape=critic.shape)
+
+    actor, critic = teacher(torch.rand([1000, 391]))
+    assert actor.shape == torch.Size([1000, 16]), "actor should be shape (1000, 16), but is {actor_shape}".format(
+        actor_shape=actor.shape)
+
+    assert critic.shape == torch.Size([1000, 1]), "critic should be shape (1000, 1), but is {critic_shape}".format(
+        critic_shape=critic.shape)
