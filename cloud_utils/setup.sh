b362d3ffdea28ed649f7f21c98f6db2c144b579b
andrew mead <ammead@umass.edu>
2023-01-12 23:32:29 -0500

    cloud training script

---
+++ cloud_utils/setup.sh
@@ -0,0 +1,15 @@
+sudo apt install megatools;
+megadl 'https://mega.co.nz/#!6f4jnDqB!b_z5kvu8yfmmRdvxfAbSgXa69QSAOcWlkiMCEFTxJ6M';
+tar -xvzf IsaacGym_Preview_4_Package.tar.gz;
+cd isaacgym/python;
+pip3 install -e .;
+cd ../..;
+git clone https://github.com/rbapat/rl-loco;
+AndrewMead10;
+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;
+cd rl-loco;
+pip install -e .;
+sudo apt-get install libpython3.7;
+Y;
+
+
