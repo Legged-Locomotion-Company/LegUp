a671a0717f93931aa88a742d12a19f88c096a725
Andrew Mead <ammead@umass.edu>
2023-01-15 18:12:01 -0500

    Update code to use configs properly

--- legup/train/agents/anymal.py
+++ legup/train/agents/anymal.py
@@ -1,21 +1,33 @@
-from legup.train.agents.base import BaseAgent
-from legup.train.rewards.anymal_rewards import WildAnymalReward
-
+from omegaconf import DictConfig
 import torch
 from typing import List
+
+from legup.train.agents.base import BaseAgent
+from legup.train.rewards.anymal_rewards import WildAnymalReward
+from legup.robots.Robot import Robot
 
 
 class AnymalAgent(BaseAgent):
     """Specific agent implementation for https://leggedrobotics.github.io/rl-perceptiveloco/assets/pdf/wild_anymal.pdf
     """
 
-    def __init__(self, robot_cfg, num_environments, asset_path, asset_name, train_cfg):
+    def __init__(self, robot_cfg: Robot, num_environments: int, asset_path: str, asset_name: str, train_cfg: DictConfig):
+        """Initialize wild anymal agent.
+
+        Args:
+            robot_cfg (Robot): Robot interface configuration class
+            num_environments (int): Number of environments
+            asset_path (str): Path to the robot asset
+            asset_name (str): Name of the robot asset
+            train_cfg (DictConfig): Configuration dictionary for training
+        """
         super().__init__(robot_cfg, num_environments, asset_path, asset_name)
 
         # need more stuff for reward function like train config
         self.dt = 1/60
         # (self, env, robot_config, train_config, dt: float):
-        self.reward_fn = WildAnymalReward(self.env, robot_cfg, train_cfg, dt=self.dt)
+        self.reward_fn = WildAnymalReward(
+            self.env, robot_cfg, train_cfg, dt=self.dt)
         self.robot_cfg = robot_cfg
         self.train_cfg = train_cfg
 
@@ -24,9 +36,12 @@ class AnymalAgent(BaseAgent):
     def reset_history_vec(self, idx=None):
         # 3 timesteps for history, 2 for velocity
         if idx is not None:
-            self.joint_pos_history[idx] = torch.zeros(12, 3).to(self.device)
-            self.joint_vel_history[idx] = torch.zeros(12, 2).to(self.device)
-            self.joint_target_history[idx] = torch.zeros(12, 2).to(self.device)
+            self.joint_pos_history[idx] = torch.zeros(
+                self.robot_cfg.num_joints, 3).to(self.device)
+            self.joint_vel_history[idx] = torch.zeros(
+                self.robot_cfg.num_joints, 2).to(self.device)
+            self.joint_target_history[idx] = torch.zeros(
+                self.robot_cfg.num_joints, 2).to(self.device)
         else:
             self.joint_pos_history = torch.zeros(
                 self.num_envs, self.robot_cfg.num_joints, 3).to(self.device)
@@ -45,7 +60,8 @@ class AnymalAgent(BaseAgent):
 
         # TODO: talk to rohan about the command
 
-        proprio[idx, :3] = torch.tensor([1., 0., 0.]).to(self.device)#self.command[idx]
+        proprio[idx, :3] = torch.tensor([1., 0., 0.]).to(
+            self.device)  # self.command[idx]
 
         proprio[idx, 3:6] = self.env.get_position()[idx]
         proprio[idx, 6:9] = self.env.get_linear_velocity()[idx]
@@ -63,14 +79,14 @@ class AnymalAgent(BaseAgent):
         # proprio[:, 120:133] = self.phase_gen(idx)
 
         privil[idx, :4] = self.env.get_contact_states(
-        )[idx][:, [3, 6, 9, 12]].to(torch.float)
+        )[idx][:, self.robot_cfg.foot_indices].to(torch.float)
 
         privil[idx, 4:16] = self.env.get_contact_forces(
-        )[idx][:, [3, 6, 9, 12], :].flatten(start_dim=1)
+        )[idx][:, self.robot_cfg.foot_indices, :].flatten(start_dim=1)
         # privil[idx, 16:28] = self.env.get_contact_normals()
         # privil[idx, 28, 32] = self.enc.get_frivtion_coeffs()
         privil[idx, 32:40] = self.env.get_contact_states()[idx][
-            :, [2, 3, 5, 6, 8, 9, 11, 12]].to(torch.float)
+            :, self.robot_cfg.shank_indices.extend(self.robot_cfg.thigh_indices)].to(torch.float)
 
         # TODO: add airtime
 
@@ -96,17 +112,17 @@ class AnymalAgent(BaseAgent):
     def reset_envs(self, envs):
         self.reset_history_vec(envs)
 
-    def check_termination(self) -> List[int]:
+    def check_termination(self) -> torch.Tensor:
         # Check if any rigidbodies are hitting the ground
         # 0 = rb index of body
         is_collided = torch.any(self.env.get_contact_states(), dim=-1)
 
         # Check if the robot is tilted too much
         is_tilted = torch.any(
-            torch.abs(self.env.get_orientation()) > self.train_cfg.max_tilt, dim=-1)
+            torch.abs(self.env.get_orientation()) > self.train_cfg["max_tilt"], dim=-1)
 
         # Check if the robot's movements exceed the torque limits
         is_exceeding_torque = torch.any(
-            torch.abs(self.env.get_joint_torques()) > self.train_cfg.max_torque, dim=-1)
+            torch.abs(self.env.get_joint_torques()) > self.train_cfg["max_torque"], dim=-1)
 
         return (is_collided + is_tilted + is_exceeding_torque).bool()
