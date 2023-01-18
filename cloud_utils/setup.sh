f951af746969d23003000eb30e56ec94f6282430
andrew mead <ammead@umass.edu>
2023-01-18 00:59:34 -0500

    bash script updates

--- cloud_utils/setup.sh
+++ cloud_utils/setup.sh
@@ -1,5 +1,6 @@
 sudo apt-get update;
 sudo apt-get install -y --no-install-recommends \
+    build-essential \
     libxcursor-dev \
     libxrandr-dev \
     libxinerama-dev \
@@ -25,12 +26,16 @@ cp ./isaacgym/docker/nvidia_icd.json /usr/share/vulkan/icd.d/nvidia_icd.json;
 cp ./isaacgym/docker/10_nvidia.json /usr/share/glvnd/egl_vendor.d/10_nvidia.json;
 export NVIDIA_VISIBLE_DEVICES=all;
 export NVIDIA_DRIVER_CAPABILITIES=all;
+export PATH="/home/legsuser/.local/bin:$PATH"
 megadl 'https://mega.co.nz/#!6f4jnDqB!b_z5kvu8yfmmRdvxfAbSgXa69QSAOcWlkiMCEFTxJ6M';
 tar -xvzf IsaacGym_Preview_4_Package.tar.gz;
 cd isaacgym/python;
 pip3 install -e .;
 cd ../..;
+sudo cp /opt/conda/lib/libpython3.8.so.1.0 /usr/lib/
 git clone https://AndrewMead10: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@github.com/Legged-Locomotion-Company/LegUp;
 cd LegUp;
 pip install -e .;
+cd Legup;
+pip install -r requirements.txt;
 wandb login aa97ba8812f4389ad4905d767febba2688c59671;
