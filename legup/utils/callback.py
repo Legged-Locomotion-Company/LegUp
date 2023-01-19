9c1240bd4e58061583bcb3e58165e27de6169fbf
Misha <m.lvovsky@protonmail.com>
2023-01-18 20:17:47 -0500

    fix config and get rid of complicated type structure in callback.py

--- legup/utils/callback.py
+++ legup/utils/callback.py
@@ -7,78 +7,8 @@ import numpy as np
 import cv2
 import os
 
-def create_custom_callback(base_callback):
-    class CustomCallback(base_callback):
-        """
-        A custom callback that derives from ``BaseCallback``.
 
-        :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
-        """
-
-        def __init__(self, env, training_id, root_path, verbose=0):
-            """_summary_
-
-            Args:
-                env (_type_): _description_
-                training_id (_type_): _description_
-                root_path (_type_): _description_
-                verbose (int, optional): _description_. Defaults to 0.
-            """
-
-            self.env_ = env
-            self.training_id = training_id
-
-            self.model_save_path = f'{root_path}/checkpoints/{self.training_id}'
-
-        def _on_training_start(self) -> None:
-            """
-            This method is called before the first rollout starts.
-            """
-            # print("BEFORE FIRST ROLLOUT TRAINING START)")
-
-            # os.makedirs(self.tb_dir_path, exist_ok=True)
-
-        def _on_rollout_start(self) -> None:
-            """
-            A rollout is the collection of environment interaction
-            using the current policy.
-            This event is triggered before collecting new samples.
-            """
-            # print("BEFORE ROLLOUT (ROLLOUT START)")
-            pass
-
-        def _on_step(self) -> bool:
-            """
-            This method will be called by the model after each call to `env.step()`.
-
-            For child callback (of an `EventCallback`), this will be called
-            when the event is triggered.
-
-            :return: (bool) If the callback returns False, training is aborted early.
-            """
-            
-            return True
-
-        def _on_rollout_end(self) -> None:
-            """
-            This event is triggered before updating the policy.
-            """
-
-            infos = self.locals['infos'][0]
-            for idx, name in enumerate(infos['names']):
-                self.logger.record(
-                    f"rewards/{name}", infos['terms'][idx].item())
-
-        def _on_training_end(self) -> None:
-            """
-            This event is triggered before exiting the `learn()` method.
-            """
-            pass
-    
-    return CustomCallback
-
-
-class CustomLocalCallback(create_custom_callback(BaseCallback)):
+class CustomLocalCallback(BaseCallback):
     """
     A custom callback that derives from ``BaseCallback``.
 
@@ -95,11 +25,29 @@ class CustomLocalCallback(create_custom_callback(BaseCallback)):
             verbose (int, optional): _description_. Defaults to 0.
         """
 
-        super().__init__(env, training_id, root_path, verbose)
-
-        BaseCallback.__init__(self, verbose)
         self.env_ = env
         self.training_id = training_id
+
+        self.model_save_path = f'{root_path}/checkpoints/{self.training_id}'
+
+        super().__init__(verbose)
+
+    def _on_training_start(self) -> None:
+        """
+        This method is called before the first rollout starts.
+        """
+        # print("BEFORE FIRST ROLLOUT TRAINING START)")
+
+        # os.makedirs(self.tb_dir_path, exist_ok=True)
+
+    def _on_rollout_start(self) -> None:
+        """
+        A rollout is the collection of environment interaction
+        using the current policy.
+        This event is triggered before collecting new samples.
+        """
+        # print("BEFORE ROLLOUT (ROLLOUT START)")
+        pass
 
     def _on_step(self) -> bool:
         """
@@ -121,7 +69,10 @@ class CustomLocalCallback(create_custom_callback(BaseCallback)):
         This event is triggered before updating the policy.
         """
 
-        super()._on_rollout_end()
+        infos = self.locals['infos'][0]
+        for idx, name in enumerate(infos['names']):
+            self.logger.record(
+                f"rewards/{name}", infos['terms'][idx].item())
 
         self.model.save(os.path.join(
             self.model_save_path, str(self.num_timesteps)))
@@ -133,18 +84,39 @@ class CustomLocalCallback(create_custom_callback(BaseCallback)):
         pass
 
 
-class CustomWandbCallback(create_custom_callback(WandbCallback)):
+class CustomWandbCallback(WandbCallback):
     def __init__(self, env, training_id, root_path, verbose=1):
 
+        # TODO: This is for debug, get rid of when it works
         self.debug_count = 0
 
-        super().__init__(env, training_id, root_path, verbose)
-
-        WandbCallback.__init__(self, model_save_path=self.model_save_path, verbose=verbose)
         self.env_ = env
+        self.training_id = training_id
+
+        self.model_save_path = f'{root_path}/checkpoints/{self.training_id}'
         self.video_buffer = []
 
+        WandbCallback.__init__(
+            self, model_save_path=self.model_save_path, verbose=verbose)
+
         Monitor(env)
+
+    def _on_training_start(self) -> None:
+        """
+        This method is called before the first rollout starts.
+        """
+        # print("BEFORE FIRST ROLLOUT TRAINING START)")
+
+        # os.makedirs(self.tb_dir_path, exist_ok=True)
+
+    def _on_rollout_start(self) -> None:
+        """
+        A rollout is the collection of environment interaction
+        using the current policy.
+        This event is triggered before collecting new samples.
+        """
+        # print("BEFORE ROLLOUT (ROLLOUT START)")
+        pass
 
     def _on_step(self) -> bool:
         """
@@ -156,8 +128,6 @@ class CustomWandbCallback(create_custom_callback(WandbCallback)):
 
         :return: (bool) If the callback returns False, training is aborted early.
         """
-
-        super()._on_step()
 
         self.video_buffer.append(self.env_.render())
 
@@ -176,7 +146,10 @@ class CustomWandbCallback(create_custom_callback(WandbCallback)):
             if give_debug_exception:
                 raise Exception("test_exception")
 
-        super()._on_rollout_end()
+        infos = self.locals['infos'][0]
+        for idx, name in enumerate(infos['names']):
+            self.logger.record(
+                f"rewards/{name}", infos['terms'][idx].item())
 
         # For some reason the video needs to be transposed to frames, channels, height, width
 
@@ -189,8 +162,16 @@ class CustomWandbCallback(create_custom_callback(WandbCallback)):
         for idx, name in enumerate(infos['names']):
             self.logger.record(f"rewards/{name}", infos['terms'][idx].item())
 
-        self.model.save(os.path.join(self.model_save_path, str(self.num_timesteps)))
+        self.model.save(os.path.join(
+            self.model_save_path, str(self.num_timesteps)))
 
-        wandb.save(os.path.join(self.model_save_path, '*'))#, base_path='checkpoints')
+        # , base_path='checkpoints')
+        wandb.save(os.path.join(self.model_save_path, '*'))
 
         self.video_buffer = []
+
+    def _on_training_end(self) -> None:
+        """
+        This event is triggered before exiting the `learn()` method.
+        """
+        pass
