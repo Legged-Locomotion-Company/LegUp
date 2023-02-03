7b2946981539b22c2e226b99c8702113b1e13070
Misha <m.lvovsky@protonmail.com>
2023-02-03 01:32:37 -0500

    fixed requirements and light_setup so they work

--- cloud_utils/development/light_setup.sh
+++ cloud_utils/development/light_setup.sh
@@ -1,21 +1,28 @@
-sudo apt-get install megatools
+cd /root;
 
+sudo apt-get update;
+sudo apt-get install -y \
+    megatools \
+    nvtop;
 
 megadl 'https://mega.co.nz/#!6f4jnDqB!b_z5kvu8yfmmRdvxfAbSgXa69QSAOcWlkiMCEFTxJ6M';
 tar -xvzf IsaacGym_Preview_4_Package.tar.gz;
 
-conda create --name legenv python=3.8;
+conda create -y --name legenv python=3.8;
+source /opt/conda/etc/profile.d/conda.sh; 
 conda activate legenv;
 
 cd isaacgym/python;
-pip3 install -e .;
+pip3 install --no-input -e .;
 
 cd ../..;
 
 git clone https://AndrewMead10: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@github.com/Legged-Locomotion-Company/LegUp;
 
 cd LegUp;
-pip install -e .;
-pip install -r requirements.txt;
+pip install --no-input -e . -;
+pip install --no-input -r requirements.txt;
 
 wandb login ab4474327941fce151a79358a59ee85a3377e660
+
+echo SETUP DONE
