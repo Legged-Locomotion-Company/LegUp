5cec209095e3db820d10f87e17c5caa4c6fad4eb
mishmish66 <m.lvovsky@protonmail.com>
2023-04-18 22:45:47 -0400

    Changed rewards to \@reward decorator.
    Co-authored-by: andrew mead <ammead@umass.edu>

---
+++ legup/common/rewards/__init__.py
@@ -0,0 +1,1 @@
+from legup.common.rewards.rewards import reward, calculate_reward, RewardArgs
--- legup/common/rewards/rewards.py
+++ legup/common/rewards/rewards.py
@@ -9,6 +9,8 @@ import torch
 class RewardArgs(NamedTuple):
     dynamics: AbstractDynamics
     robot: LeggedRobot
+    command: torch.Tensor
+    curriculum_factor: float
 
 
 rewards_dict: Dict[str, Callable[[RewardArgs], torch.Tensor]] = {}
@@ -30,7 +32,41 @@ def calculate_reward(reward_args: RewardArgs, reward_scale: Dict[str, float]):
 
 
 def reward(reward_func: Callable[[RewardArgs], torch.Tensor]) -> Callable[[RewardArgs], torch.Tensor]:
-    """Decorarator for reward functions. Will register with the name of the function"""
+    """
+    A decorator to register custom reward functions in a global rewards dictionary.
+
+    This decorator adds the reward function to a rewards dictionary, which can be later
+    referenced when calculating rewards. To register a custom reward function, simply
+    use the @reward decorator above its definition.
+
+    To calculate rewards using the registered functions, provide the function names
+    and corresponding scaling factors in the reward_scale dictionary argument of the
+    calculate_reward function.
+
+    Example:
+    --------
+    1. Define and register a custom reward function:
+
+        @reward
+        def my_reward(reward_args: RewardArgs) -> torch.Tensor:
+            # Your custom reward calculation
+            return reward_tensor
+
+    2. Calculate rewards using the registered function:
+
+        reward_scales = {"my_reward": scale_value, ...}
+        rewards = calculate_reward(reward_args, reward_scales)
+
+    Parameters:
+    -----------
+    reward_func : Callable[[RewardArgs], torch.Tensor]
+        The custom reward function to be registered.
+
+    Returns:
+    --------
+    Callable[[RewardArgs], torch.Tensor]
+        The same reward function passed as input, for further use in the code.
+    """
 
     rewards_dict[reward_func.__name__] = reward_func
     return reward_func
