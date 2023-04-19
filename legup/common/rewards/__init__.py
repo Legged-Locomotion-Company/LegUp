180aed618bae3cc77f129c9d1df7501c8e19cd8f
mishmish66 <m.lvovsky@protonmail.com>
2023-04-19 17:14:51 -0400

    improved the __init__.py for rewards

--- legup/common/rewards/__init__.py
+++ legup/common/rewards/__init__.py
@@ -1,1 +1,5 @@
-from legup.common.rewards.rewards import reward, calculate_reward, RewardArgs
+# Expose the reward utilities
+from .rewards_dict import reward, calculate_reward, RewardArgs
+
+# Add the built-in reward functions to the rewards dictionary
+from . import reward_funcs
